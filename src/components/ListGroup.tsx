5731e90694566390935ae2a18eb640406dcec457
shelbyclarice <shelbyclarice@gmail.com>
2024-12-03 20:12:21 -0500

    tutorial

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
 
+interface Props {
+  items: string[];
+  heading: string;
+}
 
-function ListGroup() {
+function ListGroup({ items, heading }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index 
+              ? 'list-group-item active' 
+              : 'list-group-item'
+            }
+            key={item}
+            onClick={() => {setSelectedIndex(index); }}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
